0e01ef084f2efeff6b021aec57975eca900a8d3a
eheh-dev <86131657+eheh-dev@users.noreply.github.com>
2022-02-03 22:32:14 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    app: {
+        px: '!mg',
+        token: 'OTE5OTQwNjIwNDU2NDQzOTM1.YbdHdw.SxwqZsv4aQmKz4mCK-4bbN8AX0g',
+        playing: 'by Zerio ❤️'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'DJ',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 100,
+        loopMessage: false,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
